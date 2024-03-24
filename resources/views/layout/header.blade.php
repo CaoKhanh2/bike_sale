f27016c0b471851fc16fcc44bed00a5f8af58a6b
Khánh <93071898+CaoKhanh2@users.noreply.github.com>
2024-03-24 18:24:41 +0700

    Update header.blade.php

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,7 +5,7 @@
                 class="d-inline-block align-text-center">
             XeToT
         </a>
-        <button class="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
