38c4fb7dc1a8de7bec0fb8da1d50126ffb9467f1
Kijiwoon <kien887637@st.vimaru.edu.vn>
2024-03-24 18:15:10 +0700

    Kiên 23/4

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,15 +5,15 @@
                 class="d-inline-block align-text-center">
             XeToT
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        <button class="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">                                                                      
-                        <img src="{{ asset('Image\Icon\home.png') }}" width="30" height="24"                                                    
+                    <a class="nav-link active" aria-current="page" href="#">
+                        <img src="{{ asset('Image\Icon\home.png') }}" width="30" height="24"
                             class="img-fluid mx-auto d-block">
                         Trang chủ
                     </a>
@@ -38,8 +38,19 @@
                             class="img-fluid mx-auto d-block">
                         Thông báo
                     </a>
-                </li>
-                <li class="nav-item">
+                </li>-
+            </ul>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                    <a class="nav-link active" aria-disabled="true" href="">
+                        <img src="{{ asset('Image\Icon\trade.png') }}" width="30" height="24"
+                            class="img-fluid mx-auto d-block">
+                        Đăng ký bán xe
+                    </a>
+            </li>
+            <li class="nav-item">
                     <a class="nav-link active" aria-disabled="true" href="">
                         <img src="{{ asset('Image\Icon\user.png') }}" width="30" height="24"
                             class="img-fluid mx-auto d-block">
