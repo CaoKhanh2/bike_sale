46d99ca8a0532c928c3ff6fe5f23bb0c17a0faa5
Kijiwoon <kien887637@st.vimaru.edu.vn>
2024-03-30 12:10:42 +0700

    Co bi loi dau a zai 30/3

--- resources/views/user/posting_item.blade.php
+++ resources/views/user/posting_item.blade.php
@@ -1,67 +1,68 @@
 <div id="posting_item">
     <div class="container bg-light">
+        <div class="clearfix">
+            <div class="pull-left">
+                <h1 class="text-primary text-uppercase text-center">Đăng ký bán xe</h1>
+            </div>
+        </div>
         <div class="row">
-            <div class="col-md-4">   
+            <div class="col-md-4">
                 <div class="clearfix">
                     <div class="pull-left">
-                    <h3 class="text-black text-center h3">Hình ảnh của xe</h3>
+                        <h3 class="text-black text-center h3">Hình ảnh của xe</h3>
                     </div>
                 </div>
                 <form class="dropzone mb-3" action="#" id="my-awesome-dropzone">
                     <div class="fallback">
                         <input type="file" name="file" />
                     </div>
-                </form>       
+                </form>
             </div>
             <div class="col-md-8">
-                <div class="clearfix">
-                    <div class="pull-left">
-                    <h3 class="text-black text-center h3">Thông tin chi tiết</h3>
+                <div class="container">
+                    <div class="row justify-content-around">
+                        <form action="" class="bg-light p-3">
+                            <h1 class="text-center h3 py-3">Thông tin chi tiết</h1>
+                            <div class="form-froup">
+                                <label for="Họ và tên">Họ và tên người bán:</label>
+                                <input type="text" name="Họ và tên" id="Họ và tên" class="form-control">
+                            </div><br>
+                            <div class="form-froup">
+                                <label for="Username">Tên đăng nhập:</label>
+                                <input type="text" name="Tên đăng nhập" id="Username" class="form-control">
+                            </div><br>
+                            <div class="form-froup">
+                                <label for="Password">Mật khẩu:</label>
+                                <input type="password" name="mật khẩu" id="Password" class="form-control">
+                            </div><br>
+                            <div class="form-group">
+                                <lable for="gender">Giới tính</lable><br>
+                                <div class="form-check form-check-inline">
+                                    <input type="radio" name="gender" id="male" class="form-check-input"
+                                        checked>
+                                    <label for="male" class="form-check-label">Nam</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input type="radio" name="gender" id="female" class="form-check-input"
+                                        checked>
+                                    <label for="female" class="form-check-label">Nữ</label>
+                                </div>
+                            </div><br>
+                            <div class="form-froup">
+                                <label for="Email">Email:</label>
+                                <input type="text" name="email" id="Email" class="form-control">
+                            </div>
+                            <br>
+                            <div>
+                                <input type="submit" value="Đăng ký" class="btn-primary btn col-12">
+                            </div>
+                            <br>
+                        </form>
                     </div>
                 </div>
-                <div class="container">
-                <div class="row justify-content-around">
-                    <form action="" class="bg-light p-3">
-                        <h1 class="text-center text-uppercase h3 py-3">Đăng ký tài khoản</h1> 
-                        <div class="form-froup">
-                            <label for="Họ và tên">Họ và tên:</label>
-                            <input type="text" name="Họ và tên" id="Họ và tên" class="form-control">
-                        </div><br>
-                        <div class="form-froup">
-                            <label for="Username">Tên đăng nhập:</label>
-                            <input type="text" name="Tên đăng nhập" id="Username" class="form-control">
-                        </div><br>
-                        <div class="form-froup">
-                            <label for="Password">Mật khẩu:</label>
-                            <input type="password" name="mật khẩu" id="Password" class="form-control">
-                        </div><br>
-                        <div class="form-group">
-                            <lable for="gender">Giới tính</lable><br>
-                            <div class="form-check form-check-inline">
-                                <input type="radio" name="gender" id="male" class="form-check-input" checked>
-                                <label for="male" class="form-check-label">Nam</label>
-                            </div>  
-                            <div class="form-check form-check-inline">
-                                <input type="radio" name="gender" id="female" class="form-check-input" checked>
-                                <label for="female" class="form-check-label">Nữ</label>
-                            </div>
-                        </div><br>
-                        <div class="form-froup">
-                            <label for="Email">Email:</label>
-                            <input type="text" name="email" id="Email" class="form-control">
-                        </div>
-                        <br>
-                        <div>
-                        <input type="submit" value="Đăng ký" class="btn-primary btn col-12">
-                        </div>  
-                        <br>
-                        <a href="{{ asset('/')}}"><input type="button" value="Thoát" class="btn-primary btn col-12"></a>
-                    </form>
-                </div>
-        </div>
             </div>
-        
+
         </div>
-        
+
     </div>
 </div>
