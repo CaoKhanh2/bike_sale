d52163b6f9f48b1e0045a709861fc99fd15fceae
CaoKhanh <caovinhkhanh@gmail.com>
2024-04-02 10:02:05 +0700

    ck-02/04

--- resources/views/user/posting_item.blade.php
+++ resources/views/user/posting_item.blade.php
@@ -2,62 +2,60 @@
     <form action="">
         <div class="clearfix">
             <div class="pull-left">
-                <h1 class="text-primary text-center pb-3">Đăng ký bán xe</h1>
-            </div><br>
+                <h1 class="text-primary text-center">Đăng ký bán xe</h1>
+            </div>
         </div>
+
         <div class="row">
-            <div class="col-md-4">
-                <div class="clearfix">
-                    <div class="pull-left">
-                        <h3 class="text-black py-3 h3">Hình ảnh của xe</h3>
-                    </div>
-                </div>
-                <form class="dropzone mb-3" action="#" id="my-awesome-dropzone">
+            <div class="col-4">
+                <label for="my-awesome-dropzone">Hình ảnh của xe</label>
+                <div class="dropzone mb-3" action="#" id="my-awesome-dropzone">
                     <div class="fallback">
                         <input type="file" name="file" />
                     </div>
-                </form>
+                </div>
             </div>
-            <div class="col-md-8">
-                <div class="container">
-                    <div class="row">
-                        <form action="">
-                            <div class="form-group">
-                                <select class="form-select form-select-lg" aria-label="Loại xe">
-                                    <option value="Xemay">Xe máy</option>
-                                    <option value="Xedapdien">Xe đạp điện</option>
-                                </select>
-                            </div><br>
-                            <h1 class="h3 py-3">Thông tin chi tiết</h1>
-                            <div class="form-group">
-                                <input type="text" name="Tên hãng xe" id="Hangxe"
-                                    class="form-control form-control-lg" placeholder="Hãng xe">
-                            </div><br>
-                            <div class="form-group">
-                                <input type="text" name="Năm đăng ký" id="Namdangky"
-                                    class="form-control form-control-lg" placeholder="Năm đăng ký">
-                            </div><br>
-                            <div class="form-group">
-                                <input type="text" name="Tên hãng xe" id="Xuatxu"
-                                    class="form-control form-control-lg" placeholder="XUất xứ">
-                            </div><br>
-                            <div class="form-group">
-                                <input type="text" name="Tên hãng xe" id="Giaban"
-                                    class="form-control form-control-lg" placeholder="Giá bán">
-                            </div>
-                            <h1 class="h3 py-3">Thông tin chi tiết</h1>
-                            <div class="form-group">
-                                <input type="text" name="Tên hãng xe" id="Hangxe"
-                                    class="form-control form-control-lg" placeholder="Tiêu đề">
-                            </div><br>
-                            <div class="form-group pb-3">
-                                <textarea class="form-control" style="height: 200px"></textarea>
-                            </div>
-                            <div>
-                                <input type="submit" value="Đăng ký" class="btn-primary btn col-12">
-                            </div>
-                            <br>
-                        </form>
+            <div class="col-6">
+                <div class="row">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Loại xe</label>
+                        <select class="form-select form-select-lg" aria-label="Loại xe" id="loaixe">
+                            <option value="Xemay">Xe máy</option>
+                            <option value="Xedapdien">Xe đạp điện</option>
+                        </select>
+                    </div>
+                    <div class="mb-2">
+                        <h2>Thông tin chi tiết</h2>
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" name="Tên hãng xe" id="Hangxe" class="form-control form-control-lg"
+                            placeholder="Hãng xe">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" name="Năm đăng ký" id="Namdangky" class="form-control form-control-lg"
+                            placeholder="Năm đăng ký">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" name="Tên hãng xe" id="Xuatxu" class="form-control form-control-lg"
+                            placeholder="Xuất xứ">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" name="Tên hãng xe" id="Giaban" class="form-control form-control-lg"
+                            placeholder="Giá bán">
+                    </div>
+
+                    <div class="mb-2">
+                        <h2>Thông tin chi tiết</h2>
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" name="Tên hãng xe" id="Hangxe" class="form-control form-control-lg"
+                            placeholder="Tiêu đề">
+                    </div>
+                    <div class="mb-3">
+                        <textarea class="form-control" style="height: 200px"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <input type="submit" value="Đăng ký" class="btn-primary btn col-12">
                     </div>
                 </div>
             </div>
