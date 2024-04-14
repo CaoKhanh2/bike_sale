8228c8cd16a5220156cd6478c725befda0189f56
Kijiwoon <kien887637@st.vimaru.edu.vn>
2024-04-14 10:42:39 +0700

    nothing to show

--- resources/views/cart/cart_content.blade.php
+++ resources/views/cart/cart_content.blade.php
@@ -1,98 +1,56 @@
 <div class="container bg-light py-3 my-3">
-    <div class="row mb-3">
-        <h2>Giỏ hàng</h2>
+    <div class="row mb-3 ms-3">
+        <h3>Giỏ hàng</h3>
     </div>
     <div class="row mx-3">
+        <table class="table table-hover border border-2 align-middle">
+            <thead>
+                <tr>
+                    <th>Thông tin sản phẩm</th>
+                    <th style="width:10em">Đơn giá</th>
+                    <th style="width:10em">Số lượng</th>
+                    <th>Thành tiền</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><img src="{{ asset('Image\Xe\\XeDien\Asama ebk\xe-dap-dien-asama-ebk-do.jpg') }}"
+                        alt="Ảnh" width="120" height="120"> Mark
+                    </td>
+                    <td>Otto</td>
+                    <td>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-outline-secondary" type="button" id="btn-decrease">-</button>
+                            </div>
+
+                            <input type="text" class="form-control text-center" id="quantity-input" value="1"
+                                aria-label="Quantity">
+
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button" id="btn-increase">+</button>
+                            </div>
+                        </div>
+                    </td>
+                    <td>100000</td>
+                    <td>
+                        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
         <div class="col-md-8">
-            <table class="table table-hover border">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Thông tin sản phẩm</th>
-                        <th style="width:10em">Đơn giá</th>
-                        <th style="width:10em">Số lượng</th>
-                        <th>Thành tiền</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row"></th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>
-
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <button class="btn btn-outline-secondary" type="button"
-                                        id="btn-decrease">-</button>
-                                </div>
-
-                                <input type="text" class="form-control text-center" id="quantity-input"
-                                    value="1" aria-label="Quantity">
-
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary" type="button"
-                                        id="btn-increase">+</button>
-                                </div>
-                            </div>
-
-                        </td>
-                        <td>100000</td>
-                    </tr>
-                    <tr>
-                        <th>2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <button class="btn btn-outline-secondary" type="button"
-                                        id="btn-decrease">-</button>
-                                </div>
-
-                                <input type="text" class="form-control text-center" id="quantity-input"
-                                    value="1" aria-label="Quantity">
-
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary" type="button"
-                                        id="btn-increase">+</button>
-                                </div>
-                            </div>
-                        </td>
-                        <td>@twitter</td>
-                    </tr>
-                    <tr>
-                        <th>3</th>
-                        <td>Larry the Bird</td>
-                        <td>@twitter</td>
-                        <td>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <button class="btn btn-outline-secondary" type="button"
-                                        id="btn-decrease">-</button>
-                                </div>
-
-                                <input type="text" class="form-control text-center" id="quantity-input"
-                                    value="1" aria-label="Quantity">
-
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary" type="button"
-                                        id="btn-increase">+</button>
-                                </div>
-                            </div>
-                        </td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div class="col-md-8">
-                <a href="{{ url('/')}}" class="btn btn-primary">Tiếp tục mua hàng</a>
-            </div>
+            <a href="{{ url('/') }}" class="btn btn-primary">Tiếp tục mua hàng</a>
         </div>
+
+    </div>
+    <div class="row mx-3">
+        <div class="col-md-8"></div>
         <div class="col-md-4">
             <div class="row py-4 bg-white">
                 <div class="col-3">
-                    <p>Tổng tiền</p>
+                    <p class="fw-bold">Tổng tiền</p>
                 </div>
                 <div class="col-9 text-end">
                     <p>1000000</p>
