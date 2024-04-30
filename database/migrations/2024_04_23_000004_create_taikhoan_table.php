e3ccee36e988c9e896b206c48390577d9e7e8399
CaoKhanh <caovinhkhanh@gmail.com>
2024-04-30 17:20:30 +0700

    ck-30/04¯\_(ツ)_/¯

--- database/migrations/2024_04_23_000004_create_taikhoan_table.php
+++ database/migrations/2024_04_23_000004_create_taikhoan_table.php
@@ -15,15 +15,16 @@ return new class extends Migration
     {
         Schema::create('taikhoan', function (Blueprint $table) {
             $table->string('matk',12);
-            $table->string('manv',10);
+            $table->string('manv',10)->nullable();
             $table->string('tentaikhoan',20);
             $table->string('password',255);
             $table->string('email',35);
             $table->timestamp('email_verified_at')->nullable();
             $table->enum('phanquyen',['Quản trị viên','Nhân viên', 'Quản lý']);
             $table->tinyInteger('trangthai');
-            $table->timestamp('ngaytao')->current_timestamp();
-            $table->timestamp('ngaycapnhat');
+            // $table->timestamp('ngaytao')->current_timestamp();
+            // $table->timestamp('ngaycapnhat');
+            $table->timestamps();
             $table->rememberToken();
 
             $table->primary('matk');
